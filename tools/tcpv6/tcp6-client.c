12550d9ed845d0d57350e8b2579eadf360b392e0
Chen Wu <chenwu@espressif.com>
2020-09-27 20:35:04 +0800

    feat: add Makefile for tcpv6 test tools

---
+++ tools/tcpv6/tcp6-client.c
@@ -0,0 +1,10 @@
+/**
+ * IPv6 TCP client example
+*/
+#include <stdio.h>
+
+int main(char argc, char *argv[])
+{
+    printf("Hello, this is a tcpv6 client example\r\n");
+    return 0;
+}
---
+++ tools/tcpv6/tcp6-server.c
@@ -0,0 +1,10 @@
+/**
+ * IPv6 TCP server example
+*/
+#include <stdio.h>
+
+int main(char argc, char *argv[])
+{
+    printf("Hello, this is a tcpv6 server example\r\n");
+    return 0;
+}
