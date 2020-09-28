462183d2703595e8d5b37510870e79503338b476
Chen Wu <chenwu@espressif.com>
2020-09-28 10:36:39 +0800

    feat: add Makefile for udpv6 example

---
+++ tools/udpv6/udpv6-client.c
@@ -0,0 +1,10 @@
+/**
+ * IPv6 UDP client example
+*/
+#include <stdio.h>
+
+int main(char argc, char *argv[])
+{
+    printf("Hello, this is a udpv6 client example.\r\n");
+    return 0;
+}
---
+++ tools/udpv6/udpv6-server.c
@@ -0,0 +1,12 @@
+/**
+ * IPv6 UDP server example
+ * 
+ * @brief   Echo all the data from the client
+*/
+#include <stdio.h>
+
+int main(char argc, char *argv[])
+{
+    printf("Hello, this is a udpv6 server example.\r\n");
+    return 0;
+}
