798b8a1ddadb3f4e87250b04fac93fce27f17e85
Chen Wu <chenwu@espressif.com>
2020-09-28 11:23:48 +0800

    feat: add udpv6 server echo

--- tools/udpv6/udpv6-client.py
+++ tools/udpv6/udpv6-client.py
@@ -4,14 +4,17 @@
 
 import socket
 
-UDP_IP = "240e:3a1:2074:430:4cd8:1475:ddb5:614a"
+IP_ADDR = "240e:3a1:2074:430:4cd8:1475:ddb5:614a"
 UDP_PORT = 59652    # port of magic number (0xE904)
 
 message = "Hello, World!"
 
-print "UDP target IP:", UDP_IP
+print "UDP target IP:", IP_ADDR
 print "UDP target port:", UDP_PORT
 print "message:", message
 
-udp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-udp_socket.sendto(message, (UDP_IP, UDP_PORT))
+udp6_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+udp6_socket.sendto(message, (IP_ADDR, UDP_PORT))
+
+data, addr = udp6_socket.recvfrom(1460)
+print "udp received message from server:", data
--- tools/udpv6/udpv6-server.py
+++ tools/udpv6/udpv6-server.py
@@ -7,9 +7,11 @@ import socket
 UDP_IP = "::"
 UDP_PORT = 59652    # port of magic number
 
-udp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-udp_socket.bind((UDP_IP, UDP_PORT))
+udp6_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+udp6_socket.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = udp_socket.recvfrom(1460)
+    data, addr = udp6_socket.recvfrom(1460)
     print "udp received message:", data
+    udp6_socket.sendto(data, addr)
+    print "udp echo message to client:", data
